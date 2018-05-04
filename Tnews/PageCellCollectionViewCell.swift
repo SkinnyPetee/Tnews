4546266efbe2a90a4a18d8b79ebf739fc90e8a85
sriram <sriram@srirams-mac.local>
2018-05-04 05:03:15 -0700

    basic collectionView added

---
+++ Tnews/PageCellCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PageCellCollectionViewCell.swift
+//  Tnews
+//
+//  Created by sriram on 04/05/18.
+//  Copyright © 2018 sriram. All rights reserved.
+//
+
+import UIKit
+
+class PageCellCollectionViewCell: UICollectionViewCell {
+    
+    
+    
+    
+}
--- Tnews/ViewController.swift
+++ Tnews/ViewController.swift
@@ -8,18 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak  var collectionView: UICollectionView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.clipsToBounds = true
+        collectionView.isPagingEnabled = true
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
     }
-
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellID", for: indexPath)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: view.frame.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    
 }
 
